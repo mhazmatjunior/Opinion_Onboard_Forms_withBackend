b61170774f8c155df5416d757d3f2799f7738813
Muhammad Hassan Azmat <mhazmatjunior@gmail.com>
2026-01-15 15:31:35 +0500

    Finished Project
     Using useActionState and useOptimistic

---
+++ src/components/Submit.jsx
@@ -0,0 +1,9 @@
+export default function Submit({ pending }) {
+  return (
+    <p className="actions">
+      <button type="submit" disabled={pending}>
+        {pending ? "Submitting..." : "Submit"}
+      </button>
+    </p>
+  );
+}
